bfa4ffe42a5330c66480cef6fb3f92c09cbb57b0
drugdieitrighthere <ducdai03.private@gmail.com>
2026-04-08 18:17:15 +0700

    feat: add domain entities: Newspaper, ContentSegment, Podcast, QuoteOfDay, WordOfDay

--- src/main/java/com/chirplingo/domain/newspaper/ContentSegment.java
+++ src/main/java/com/chirplingo/domain/newspaper/ContentSegment.java
@@ -1,5 +1,19 @@
 package com.chirplingo.domain.newspaper;
 
 public class ContentSegment {
-    
+    private String en;
+    private String vn;
+    private int  orderIndex;
+    public ContentSegment(String en, String vn, int orderIndex) {
+        this.en = en;
+        this.vn = vn;
+        this.orderIndex = orderIndex;
+    }
+
+    public String getEn(){
+        return en;
+    }
+    public String getVn(){
+        return vn;
+    }
 }
